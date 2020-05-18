539cf47a0914800d214f66e717b25eebf17f447e
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-05-18 16:04:11 -0600

    added trivia questions and created loop to cycle through

---
+++ assests/javascript/app.js
@@ -0,0 +1,112 @@
+var triviaQuestions = [{
+    question: "What is the capital of Vermont?",
+    answers: ["Austin", "Nashville", "Montepelier", "Pierre"],
+    correct: "Montepelier"
+},
+{
+    question: "What is the capital of Washington?",
+    answers: ["Chareleston", "Madison", "Cheyenne", "Olympia"],
+    correct: "Olympia"
+},
+
+{
+    question: "What is the capital of Maryland?",
+    answers: ["Annapolis", "Augusta", "Springfield", "Boise"],
+    correct: "Annapolis"
+},
+
+{
+    question: "What is the capital of Michigan?",
+    answers: ["St. Paul", "Jackson", "Jefferson City", "Lansing"],
+    correct: "Lansing"
+},
+{
+    question: "What is the capital of Colorado?",
+    answers: ["Honolulu", "Frankfort", "Denver", "Trenton"],
+    correct: "Denver"
+},
+{
+    question: "What is the capital of Oregon?",
+    answers: ["Salem", "Raleigh", "Columbus", "Santa Fe"],
+    correct: "Salem"
+},
+{
+    question: "What is the capital of California?",
+    answers: ["Sacremento", "Albany", "Jackson", "Boise"],
+    correct: "Sacremento"
+},
+{
+    question: "What is the capital of Alaska?",
+    answers: ["Annapolis", "Juneau", "Little Rock", "Dover"],
+    correct: "Juneau"
+},
+{
+    question: "What is the capital of Connecticut?",
+    answers: ["Tallahhassee", "Phoenix", "Hartford", "Montgomery"],
+    correct: "Hartford"
+},
+{
+    question: "What is the capital of Hawaii?",
+    answers: ["Carson City", "Honolulu", "Des Moines", "Atlanta"],
+    correct: "Honolulu"
+}]
+
+let questionPosition = 0; 
+let totalCorrect = 0; 
+
+function makeQuestion () {
+
+    $("#questions").empty();
+    let triviaQuestion = triviaQuestions[questionPosition];
+
+    let header = $(`<h2>${triviaQuestion.question}</h2>`)
+    let answers = triviaQuestion.answers;
+    let divContainer = $(`<div id=${questionPosition}>`)
+    let button = $(`<button id='submit' data-question=${questionPosition}>Submit</button>`)
+    divContainer.append(header);
+    
+    for(let j = 0; j < answers.length; j++) {
+        let answer = answers[j];
+        let radioInput = $(`<input type=radio id='answer-${j+1}-q-${questionPosition + 1}' name='question-${questionPosition+1}' value=${answer}
+        />`)
+        let label = $("<label for='dewey'>").text(answer)
+        divContainer.append(radioInput, label)
+    }
+
+    button.click((e)=> {
+        e.preventDefault(); 
+        let submit = $(e.target);
+        let position = submit.data().question;
+    
+        let question = triviaQuestions[position]
+        let correct = question.correct; 
+        let input = $(`input[name='question-${questionPosition+1}']:checked`).val();
+        if(correct === input) {
+            totalCorrect++; 
+            console.log("Yay, you guessed");
+        } else {
+            console.log("Guessed wrong", correct, input);
+        }
+        questionPosition = questionPosition + 1;
+        if(questionPosition >= triviaQuestions.length) {
+            console.log("Game OVer, your total is: ", totalCorrect);
+
+            return; 
+        } else {
+            makeQuestion();
+        }
+        
+    })
+
+    $("#questions").append(divContainer, button)
+
+}
+makeQuestion();
+
+
+
+
+
+
+
+
