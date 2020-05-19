99229cab8bca2eb4ddbdeb61b20807289fa68175
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-05-19 15:36:29 -0600

    created scoring feature

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -19,40 +19,48 @@ var triviaQuestions = [{
     question: "What is the capital of Michigan?",
     answers: ["St. Paul", "Jackson", "Jefferson City", "Lansing"],
     correct: "Lansing"
-},
-{
-    question: "What is the capital of Colorado?",
-    answers: ["Honolulu", "Frankfort", "Denver", "Trenton"],
-    correct: "Denver"
-},
-{
-    question: "What is the capital of Oregon?",
-    answers: ["Salem", "Raleigh", "Columbus", "Santa Fe"],
-    correct: "Salem"
-},
-{
-    question: "What is the capital of California?",
-    answers: ["Sacremento", "Albany", "Jackson", "Boise"],
-    correct: "Sacremento"
-},
-{
-    question: "What is the capital of Alaska?",
-    answers: ["Annapolis", "Juneau", "Little Rock", "Dover"],
-    correct: "Juneau"
-},
-{
-    question: "What is the capital of Connecticut?",
-    answers: ["Tallahhassee", "Phoenix", "Hartford", "Montgomery"],
-    correct: "Hartford"
-},
-{
-    question: "What is the capital of Hawaii?",
-    answers: ["Carson City", "Honolulu", "Des Moines", "Atlanta"],
-    correct: "Honolulu"
 }]
+// {
+//     question: "What is the capital of Colorado?",
+//     answers: ["Honolulu", "Frankfort", "Denver", "Trenton"],
+//     correct: "Denver"
+// },
+// {
+//     question: "What is the capital of Oregon?",
+//     answers: ["Salem", "Raleigh", "Columbus", "Santa Fe"],
+//     correct: "Salem"
+// },
+// {
+//     question: "What is the capital of California?",
+//     answers: ["Sacremento", "Albany", "Jackson", "Boise"],
+//     correct: "Sacremento"
+// },
+// {
+//     question: "What is the capital of Alaska?",
+//     answers: ["Annapolis", "Juneau", "Little Rock", "Dover"],
+//     correct: "Juneau"
+// },
+// {
+//     question: "What is the capital of Connecticut?",
+//     answers: ["Tallahhassee", "Phoenix", "Hartford", "Montgomery"],
+//     correct: "Hartford"
+// },
+// {
+//     question: "What is the capital of Hawaii?",
+//     answers: ["Carson City", "Honolulu", "Des Moines", "Atlanta"],
+//     correct: "Honolulu"
+// }]
+
+
+var correct = 0;
+var incorrect = 0;
+
 
 let questionPosition = 0; 
-let totalCorrect = 0; 
+let totalCorrect = 0;
+
+let ci = $("#correct-incorrect");
+ci.css('display', 'none')
 
 function makeQuestion () {
 
@@ -81,27 +89,72 @@ function makeQuestion () {
         let question = triviaQuestions[position]
         let correct = question.correct; 
         let input = $(`input[name='question-${questionPosition+1}']:checked`).val();
+        if(!input) {
+            $("#feedback").text("You have to select and option")
+            return; 
+        }
         if(correct === input) {
             totalCorrect++; 
+            answerCorrect();
             console.log("Yay, you guessed");
         } else {
             console.log("Guessed wrong", correct, input);
+            answerIncorrect();
         }
+        $("#feedback").empty();
+
         questionPosition = questionPosition + 1;
         if(questionPosition >= triviaQuestions.length) {
+            $("#endGameInfo").text(`Game Over`)
+            $("#begin").css('display', 'inline-block')
             console.log("Game OVer, your total is: ", totalCorrect);
 
             return; 
         } else {
             makeQuestion();
         }
-        
+        //
     })
 
     $("#questions").append(divContainer, button)
 
+
+
+
+
 }
-makeQuestion();
+
+
+$("#begin").click((e)=>{
+    let target = e.target;
+    correct = 0;
+    incorrect = 0; 
+    totalCorrect = 0;
+    questionPosition = 0;
+    reset(target);
+    makeQuestion();
+    
+
+} )
+function reset(target) {
+    $(target).css('display', 'none')
+    $("#endGameInfo").empty();
+    $('#correct').text(correct);
+    $('#incorrect').text(incorrect);
+    $("#correct-incorrect").css('display', 'block')
+}
+
+function answerCorrect() {
+    correct = correct + 1;
+    $('#correct').text(correct);
+
+}
+
+function answerIncorrect() {
+    incorrect = incorrect + 1;
+    $('#incorrect').text(incorrect);
+}
+//makeQuestion();
 
 
 
